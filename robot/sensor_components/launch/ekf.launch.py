a6399bdbb6bac0f5dac044f5061e93a967ced02c
MinjeKang73 <kmj@tukorea.ac.kr>
2026-04-06 22:58:44 +0900

    [KMJ] feat: Add robot size information to description

---
+++ robot/sensor_components/launch/ekf.launch.py
@@ -0,0 +1,20 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    pkg_share = get_package_share_directory('sensor_components')
+    ekf_config = os.path.join(pkg_share, 'config', 'ekf.yaml')
+
+    return LaunchDescription([
+        Node(
+            package='robot_localization',
+            executable='ekf_node',
+            name='ekf_filter_node',
+            output='screen',
+            parameters=[ekf_config]
+        )
+    ])
